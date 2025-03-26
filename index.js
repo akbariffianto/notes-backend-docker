5fe8c721f05cd6a5b683de4755fcd54f83988443
akbararif1103 <123220004@student.upnyk.ac.id>
2025-03-26 11:41:31 +0700

    update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import route from "./routes/route.js";
+import route from "./routes/Route.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
