1e86f824e36ae652f767a34c22498a8cbff0d700
akbararif1103 <123220004@student.upnyk.ac.id>
2025-03-26 09:54:16 +0700

    update for tugas 6

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import cors from "cors";
-import route from "./routes/Route.js";
+import route from "./routes/route.js";
 
 const app = express();
+const port = process.env.PORT | 5000;
+
 app.use(cors());
 app.use(express.json());
 app.use(route);
 
-app.listen(5000, () =>console.log('Server is running on port 5000'));
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
