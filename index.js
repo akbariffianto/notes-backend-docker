bb7fa86153c3efb8defb1e8cf0da0272b8218425
akbararif1103 <123220004@student.upnyk.ac.id>
2025-05-13 22:51:19 +0700

    trying on local FE

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ const port = process.env.PORT || 5000;
 // Konfigurasi CORS agar mengizinkan domain frontend terdeploy
 const corsOptions = {
  origin: [
-    "https://notes-frontend-wijdan-dot-projek-tcc-1.uc.r.appspot.com",
-    "http://localhost:3000", // Tambahkan localhost untuk pengembangan lokal
+    "http://localhost:5173", // Tambahkan localhost untuk pengembangan lokal
   ], // Ganti dengan URL frontend yang terdeploy
   credentials: true, // Memungkinkan penggunaan cookies
   methods: ["GET", "POST", "PUT", "DELETE"],
