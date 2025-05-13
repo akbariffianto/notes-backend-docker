d75f9e30627308f9ee3c80897ba24332efecad6b
akbararif1103 <123220004@student.upnyk.ac.id>
2025-05-13 23:02:29 +0700

    trying on local FE

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
 import express from "express";
 import cors from "cors";
 import route from "./routes/Route.js";
-import "./model/index.js"; // <- PENTING: memicu sinkronisasi DB
+import "./model/index.js";
 import cookieParser from "cookie-parser";
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Konfigurasi CORS agar mengizinkan domain frontend terdeploy
 const corsOptions = {
  origin: [
-    "http://localhost:5173", // Tambahkan localhost untuk pengembangan lokal
-  ], // Ganti dengan URL frontend yang terdeploy
-  credentials: true, // Memungkinkan penggunaan cookies
+    "http://localhost:5173",
+  ], 
+  credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 
-app.use(cors(corsOptions)); // Menggunakan opsi CORS
-
-// Menambahkan penanganan preflight request (OPTIONS)
-app.options("*", cors(corsOptions)); // Menanggapi preflight requests
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions)); 
 
 app.use(express.json());
 app.use(cookieParser());
